f2ce0915a0105ca565c7a589ed86c92de196487a
Tomymanoukian <tomymanoukian@gmail.com>
2024-09-19 18:25:05 -0300

    Funciona Ej:2-d

--- ejercicio2d.m
+++ ejercicio2d.m
@@ -29,35 +29,32 @@ A6 = Aintegrar(U6,intervalo_1sigma,N(6),mu,sigma);
 A = [A1,A2,A3,A4,A5,A6];
 
 % Mostrar resultados
-fprintf('Probabilidad P(µ - σ < X < µ + σ) con N =%d: %f\n',N(1), A(1));
-fprintf('Probabilidad P(µ - σ < X < µ + σ):con N =%d: %f\n',N(2), A(2));
-fprintf('Probabilidad P(µ - σ < X < µ + σ):con N =%d: %f\n',N(3), A(3));
-fprintf('Probabilidad P(µ - σ < X < µ + σ):con N =%d: %f\n',N(4) ,A(4));
-fprintf('Probabilidad P(µ - σ < X < µ + σ):con N =%d: %f\n',N(5) ,A(5));
-fprintf('Probabilidad P(µ - σ < X < µ + σ):con N =%d: %f\n',N(6) ,A(6));
-
-dif = Zeros(6);
-
-for i= 1:6
-    dif(i) = Difrencia(U1,intervalo_1sigma,N(i),mu,sigma);
+for i = 1:length(N)
+    fprintf('Probabilidad P(µ - σ < X < µ + σ) con N =%d: %f\n',N(i), A(i));
 end
 
+
+MSE1 = errorCuadraticoMedio(U1,intervalo_1sigma,N(1),mu,sigma);
+MSE2 = errorCuadraticoMedio(U2,intervalo_1sigma,N(2),mu,sigma);
+MSE3 = errorCuadraticoMedio(U3,intervalo_1sigma,N(3),mu,sigma);
+MSE4 = errorCuadraticoMedio(U4,intervalo_1sigma,N(4),mu,sigma);
+MSE5 = errorCuadraticoMedio(U5,intervalo_1sigma,N(5),mu,sigma);
+MSE6 = errorCuadraticoMedio(U6,intervalo_1sigma,N(6),mu,sigma);
+
+
+function MSE = errorCuadraticoMedio(U,intervalo,n,mu,sigma)
+    MSE = 0;
+    for k = 1:50
+        MSE = MSE + (Aintegrar(U,intervalo,n,mu,sigma)-0.682687273250961)^2;
+    end
+    MSE = MSE/50;
+end
 
 
 function I = Aintegrar(U,intervalo,n,mu,sigma)
     f = (1/(sqrt(2*sigma*pi)))*exp(-((U-mu).^(2))/(2*sigma));
     a = intervalo(2) - intervalo(1);
     I = a*(sum(f))/n;
-end
-
-function dif = Difrencia(U,intervalo,n,mu,sigma) 
-    a = zeros(50);
-    
-    for k = 1:50
-        a(k) = Aintegrar(U,intervalo,n,mu,sigma)-0.682687273250961;
-    end
-    
-    dif= a^2;
 end
 
 
